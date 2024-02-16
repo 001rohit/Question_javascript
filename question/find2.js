c38c2484e69facb31f186fffc3e4412a176e0df9
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-02-16 19:22:33 +0530

    practice question

---
+++ question/find2.js
@@ -0,0 +1,77 @@
+
+//<------------- factorial number series 1 to 10------------->
+
+// array1 = [1,2,3,4,5,6,7,8,9,10]
+// let fact = 1
+// let num = 5;
+// for(let a =num;a>0;a--){
+//         fact*=a;
+//     }
+// console.log(fact)
+
+//<------------- sum of Even number------------->
+
+// array1 = [1,2,3,4]
+// let sumEven =0;
+// for(let a = 0 ; a<array1.length;a++){
+//     //  console.log(array1[a])
+//     if(array1[a]%2==0){
+//       sumEven +=array1[a]
+//     }
+// }
+// console.log(sumEven)
+
+
+//<------------- sum of Odd number------------->
+
+// array1 = [1,2,3,4]
+// let sumOdd =0;
+// for(let a = 0 ; a<array1.length;a++){
+//     //  console.log(array1[a])
+//     if(array1[a]%2!=0){
+//       sumOdd +=array1[a]
+//     }
+// }
+// console.log(sumOdd)
+
+
+//<------------- sum of all number------------->
+// array1 = [1,2,3,4]
+// let sum =0;
+// for(let a = 0 ; a<array1.length;a++){
+//       sum +=array1[a]
+// }
+// console.log(sum)
+
+
+//<------------- find all factorial number------------->
+
+// array1 = [1,2,3,4,5,6,7,8,9,10]
+
+// array1.forEach(element => {
+//      let fact=1;
+//     for(let a = element;a>0;a--){
+//         fact*=a;
+//     }
+//     console.log("Factorial",fact)
+// });
+
+//<-------------  all  square number------------->
+
+// array1 = [1,2,3,4,5,6,7,8,9,10]
+// array1.forEach(element => {
+// console.log(element,element*element)
+// });
+
+
+//<------------- all qube number------------->
+
+// array1 = [1,2,3,4,5,6,7,8,9,10]
+
+// array1.forEach(element => {
+//     console.log(element,element*element*element)
+//     });
+
+
+
+
---
+++ question/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+if(array1[a]%2==0){
+    //   prime +=1
+    // }
+    // console.log(prime)
