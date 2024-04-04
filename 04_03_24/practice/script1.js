cc15119e7046fc774136aa858273d68cf3a9402d
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-04-04 22:24:15 +0530

    question practice

---
+++ 04_03_24/practice/script1.js
@@ -0,0 +1,25 @@
+const x = 10
+console.log(x);
+// x = 20
+// console.log(x);
+
+// let a = 100
+// console.log(a);
+// let a = 20
+// console.log(a);
+//     function myfunction(){
+//         console.log(a);
+//     }
+//     myfunction()
+
+// function myfirst(){
+//     var b =101
+//     console.log(b);
+// }
+// myfirst()
+
+// console.log(c);
+// var c=10
+
+// operator -> it is a symbole that, performs some operation
+
