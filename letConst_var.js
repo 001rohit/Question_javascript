00814cdd2860867e3bf4a83612ffe0f8b901b076
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-04-11 20:45:34 +0530

    11_04_24

---
+++ letConst_var.js
@@ -0,0 +1,58 @@
+
+
+// let a=2;
+// a=3;
+// console.log(a);
+
+// let b=3;
+// console.log(a);
+
+// const a=4
+// a=1
+// console.log(a);
+
+// {
+//     let a=4
+//     const b=5
+//     var c =10
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(c);
+
+// function myfunction(){
+//     const b=4
+//     var  c = 30
+//     let a =2
+//     function second(){
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//       }
+//       second()
+//     }
+
+    // console.log(c);
+// console.log(a);
+// console.log(b);
+// myfunction()
+
+// callback
+
+// function myCallBack(a,b,operator){
+//     return operator(a,b)
+// }
+// function sum(a,b){
+//     return a+b
+// }
+// function sub(a,b){
+//     return a-b
+// }
+
+// let sum1 = myCallBack(5,3,sum)
+// let sub1 = myCallBack(5,3,sub)
+
+// console.log(sum1);
+// console.log(sub1);
+
+// callBack function is a function to use to call  one to another function.It is a callBack function
---
+++ loop.js
@@ -0,0 +1,11 @@
+
+let  a =0
+let b=1;
+let c;
+console.log(a);
+console.log(b);
+for(let i=0 ;i<=7;i++){
+    c = a+b
+    console.log(c);
+    
+}
